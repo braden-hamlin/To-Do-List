78d8060444edf392f541660fdd4b6d05b9a6c348
Braden Hamlin <braden@placefull.com>
2019-10-13 17:20:14 -0700

    5.3 complete

---
+++ To Do List/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  To Do List
+//
+//  Created by Braden Hamlin on 10/13/19.
+//  Copyright © 2019 Braden Hamlin. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var toDoField: UITextField!
+    var toDoItem: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let toDoItem = toDoItem {
+            toDoField.text = toDoItem
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "UnwindFromSave" {
+            toDoItem = toDoField.text
+    }
+    }
+    @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+
+}
+
